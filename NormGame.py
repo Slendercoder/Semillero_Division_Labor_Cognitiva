e4bb1a50f49c1b89292b8d547a703af8facb07f7
Oscargo01 <oscarand.gomez@urosario.edu.co>
2018-04-12 13:14:09 -0500

    Creacion de jugadores

---
+++ NormGame.py
@@ -0,0 +1,14 @@
+from random import uniform
+class Jugadores():
+    def __init__(Self, S, B, V):
+        self.Score = S
+        self.Boldness = B
+        self.Vengefulness = V
+
+def Inicializar:
+    Personas = []
+    for i in Jugadores:
+        add.Jugadores(0, uniform(0,1),uniform(0,1))
+
+a = Personas[0].Boldness
+print(a)
