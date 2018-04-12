faa2b9e211bb95c4e41194e04b7086436dfc747e
Slendercoder <andradeed78@gmail.com>
2018-04-12 13:41:53 -0500

    Funcion de imprimir y primera correccion

--- NormGame.py
+++ NormGame.py
@@ -1,22 +1,30 @@
 from random import uniform
 from random import randint
+
 class Jugadores():
     def __init__(self, S, B, V):
         self.Score = S
         self.Boldness = B
         self.Vengefulness = V
 
+def Imprimir_Jugador(j, Personas):
+    print "Score: " + str(Personas[j].Score)
+    print "Boldness: " + str(Personas[j].Boldness)
+    print "Vengefulness: " + str(Personas[j].Vengefulness)
+
 
 
 Personas = []
 
-for i in range(0,21):
+for i in range(0,20):
     Personas.append(Jugadores(0, uniform(0,1),uniform(0,1)))
 
 
+Imprimir_Jugador(19, Personas)
+
 for j in Personas:
     s = uniform(0,1)
-    b = Personas[j].Boldness
+    b = j.Boldness
     if s<b:
         print(Personas[j].Score)
         Personas[j].Score +=3
@@ -24,11 +32,3 @@ for j in Personas:
         for y in Personas and y!=j:
             Personas[y].Score += -1
             print(Personas[y].Score)
-
-
-
-
-
-       
-
-
