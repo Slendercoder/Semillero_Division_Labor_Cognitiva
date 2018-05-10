93d0d34d70b4ac51f546229ebdc430171ab09b39
Slendercoder <andradeed78@gmail.com>
2018-05-10 09:00:04 -0500

    Primera version promedio 100 experimentos

--- NormGame.py
+++ NormGame.py
@@ -5,6 +5,7 @@ from random import randint
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import scipy as sp
 
 # *******************************************************************
 # DEFINICIONES DE OBJETOS Y FUNCIONES
@@ -22,7 +23,6 @@ def Imprimir_Jugador(j, Personas):
     print("Vengefulness: " + str(Personas[j].Vengefulness))
 
 def Iteracion(Poblacion):
-    Sigue_norma=[]
     Corrompe_norma=[]
     for u in Poblacion:
         #Le damos a cada habitante 4 oportunidades para no cumplir la norma
@@ -45,104 +45,152 @@ def Iteracion(Poblacion):
                 Corrompe_norma.append(0)
     return Poblacion, Corrompe_norma
 
+def Experimento(Boldness_inicial, Vengefulness_inicial):
+    # Inicializamos las variables
+    Personas = []
+    Scores =[]
+    Boldness_1 =[]
+    Vengefulness_1 = []
+    Corrompe_norma_1 = []
+
+    # Creamos la poblacion inicial donde cada habitante tiene un nivel aleatorio de boldness y vengefulness
+    for i in range(0,20):
+        Bold = sp.random.normal(Boldness_inicial, 0.2, 1)[0]
+        if Bold > 1:
+            Bold = 1
+        elif Bold < 0:
+            Bold = 0
+
+        Veng = sp.random.normal(Vengefulness_inicial, 0.2, 1)[0]
+        if Veng > 1:
+            Veng = 1
+        elif Veng < 0:
+            Veng = 0
+
+        Personas.append(Jugadores(0, Bold, Veng))
+
+    #Creamos un bucle para iterar 100 nuevas generaciones
+    print "Corriendo iteraciones..."
+    for y in range(0,NumGeneraciones):
+
+        Personas , aux = Iteracion(Personas)
+
+        Scores.append([u.Score for u in Personas])
+        Boldness_1.append([u.Boldness for u in Personas])
+        Vengefulness_1.append([u.Vengefulness for u in Personas])
+        Corrompe_norma_1.append(aux)
+
+        # Halla el promedio del Score y su desvacion estándar
+        M = np.mean(Scores)
+        # print "El promedio de scores es: " + str(M)
+        std_deviation = np.std(Scores)
+        # print "La desv. est. de scores es: " + str(std_deviation)
+
+        # M = np.mean(Boldness_1)
+        # print "El promedio de boldness es: " + str(M)
+        #
+        # M = np.mean(Vengefulness_1)
+        # print "El promedio de vengefulness es: " + str(M)
+
+
+        # Identifica a los buenos y a los regulares
+        indices_buenos = []
+        indices_regulares = []
+
+        for i in range(len(Personas)):
+            x = (Personas[i].Score-M)/std_deviation
+            if (x) >= 1: indices_buenos.append(i)
+            elif x > -1: indices_regulares.append(i)
+
+        # print "Lista de buenos (tamano " + str(len(indices_buenos)) + ")"
+        # print indices_buenos
+        # print "Lista de regulares (tamano " + str(len(indices_regulares)) + ")"
+        # print indices_regulares
+
+        # Se crea una nueva lista dependiendo de la descendencia de los buenos y los 			regulares
+        Personas_nuevas =[]
+        for i in indices_buenos:
+            Personas_nuevas.append(Personas[i])
+            Personas_nuevas.append(Personas[i])
+
+        # print "tamano de los nuevos buenos: " + str(len(Personas_nuevas))
+
+        for i in indices_regulares:
+            Personas_nuevas.append(Personas[i])
+
+        # print "Tamano de los nuevos regulares: " + str(len(Personas_nuevas))
+
+        if len(Personas_nuevas)<=20:
+            Personas = Personas_nuevas
+            x = 20 - len(Personas)
+            for i in range(x):
+                Bold = sp.random.normal(Boldness_inicial, 0.2, 1)[0]
+                if Bold > 1:
+                    Bold = 1
+                elif Bold < 0:
+                    Bold = 0
+                Veng = sp.random.normal(Vengefulness_inicial, 0.2, 1)[0]
+                if Veng > 1:
+                    Veng = 1
+                elif Veng < 0:
+                    Veng = 0
+                Personas.append(Jugadores(0, Bold, Veng))
+        else:
+            Personas = Personas_nuevas[:20]
+
+    x = [np.mean(u) for u in Scores]
+    y = [np.mean(u) for u in Boldness_1]
+    z = [np.mean(u) for u in Vengefulness_1]
+    u = [np.mean(u) for u in Corrompe_norma_1]
+
+    return x, y, z, u
 
 # *******************************************************************
 # PARAMETROS DEL MODELO
 # *******************************************************************
 
-NumGeneraciones = 100
+NumExp = 100
+NumGeneraciones = 500
+Boldness_inicial = 0.25
+Vengefulness_inicial = 0.75
 
 # *******************************************************************
 
-# Inicializamos las variables
-Personas = []
-Scores =[]
-Boldness_1 =[]
-Vengefulness_1 = []
-Corrompe_norma_1 = []
+X = []
 
-# Falta inicializar lista de Boldness y Vengefulness
+# Esto dentro de un bucle
+x, y, z, u = Experimento(0.25, 0.75)
+X.append(x)
 
-# Creamos la poblacion inicial donde cada habitante tiene un nivel aleatorio de boldness y vengefulness
-for i in range(0,20):
-    Personas.append(Jugadores(0, 0.75 ,0.75 ))
-#Creamos un bucle para iterar 100 nuevas generaciones
-print "Corriendo iteraciones..."
-for y in range(0,NumGeneraciones):
+# Hallamos los promedios por experimento
+x = []
+for i in range(NumGeneraciones):
+    gen = [e[i] for e in X]
+    x.append(np.mean(gen))
 
-    Personas , Corrompe_norma_1= Iteracion(Personas)
-
-    Scores.append([u.Score for u in Personas])
-    Boldness_1.append([u.Boldness for u in Personas])
-    Vengefulness_1.append([u.Vengefulness for u in Personas])
-
-
-    # Halla los promedios de los Scores, Vengefulness y Boldness y halla la desvacion 			estándar de los Scores
-    M = np.mean(Scores)
-    print "El promedio de scores es: " + str(M)
-    std_deviation = np.std(Scores)
-    print "La desv. est. de scores es: " + str(std_deviation)
-
-    # M = np.mean(Boldness_1)
-    # print "El promedio de boldness es: " + str(M)
-    #
-    # M = np.mean(Vengefulness_1)
-    # print "El promedio de vengefulness es: " + str(M)
-
-
-    # Identifica a los buenos y a los regulares
-    indices_buenos = []
-    indices_regulares = []
-
-    for i in range(len(Personas)):
-        x = (Personas[i].Score-M)/std_deviation
-        if (x) >= 1: indices_buenos.append(i)
-        elif x > -1: indices_regulares.append(i)
-
-    print "Lista de buenos (tamano " + str(len(indices_buenos)) + ")"
-    print indices_buenos
-    print "Lista de regulares (tamano " + str(len(indices_regulares)) + ")"
-    print indices_regulares
-
-    # Se crea una nueva lista dependiendo de la descendencia de los buenos y los 			regulares
-    Personas_nuevas =[]
-    for i in indices_buenos:
-        Personas_nuevas.append(Personas[i])
-        Personas_nuevas.append(Personas[i])
-
-    print "tamano de los nuevos buenos: " + str(len(Personas_nuevas))
-
-    for i in indices_regulares:
-        Personas_nuevas.append(Personas[i])
-
-    print "Tamano de los nuevos regulares: " + str(len(Personas_nuevas))
-
-    if len(Personas_nuevas)<=20:
-        Personas = Personas_nuevas
-        x = 20 - len(Personas)
-        for i in range(x):
-            Personas.append(Jugadores(0, 0.75 ,0.75))
-    else:
-        Personas = Personas_nuevas[:20]
-print Corrompe_norma_1
-print "Listo!"
+# print Corrompe_norma_1
+# print "Listo!"
 
 print "Dibujando..."
-f, axarr = plt.subplots(3, sharex=True)
+# f, axarr = plt.subplots(4, sharex=True)
+f, axarr = plt.subplots(4)
 
 
 axarr[0].set_ylabel('Score')
 axarr[1].set_ylabel('Boldness')
+axarr[1].set_ylim(-0.1, 1.1)
 axarr[2].set_ylabel('Vengefulness')
-axarr[2].set_xlabel('Generation')
+axarr[2].set_ylim(-0.1, 1.1)
+axarr[3].set_ylabel('Norma')
+axarr[3].set_ylim(-0.1, 1.1)
+axarr[3].set_xlabel('Generation')
 
-x = [np.mean(u) for u in Scores]
-y = [np.mean(u) for u in Boldness_1]
-z = [np.mean(u) for u in Vengefulness_1]
-
-axarr[0].plot( x)
-axarr[1].plot( y, 'y--', linewidth = 1)
-axarr[2].plot( z, 'r-.', linewidth = 1)
+axarr[0].plot(x)
+axarr[1].plot(y)
+axarr[2].plot(z)
+axarr[3].plot(u)
+# axarr[1].plot( y, 'y--', linewidth = 1)
+# axarr[2].plot( z, 'r-.', linewidth = 1)
 axarr[0].set_title('')
 
 # axarr[1].scatter(x, y)
